b39d5c7dbefc16c74caf5f2688bb0bf80c196829
Sound Spinning <soundspinning@hotmail.com>
2019-09-25 11:12:48 +0100

    Update audio.js

--- js/audio.js
+++ js/audio.js
@@ -7,25 +7,28 @@ $(document).ready(function(){
 });
 
 // 
-// Play/Pause tracks with single play click
-// Note: it works on iOS devices
+// Play/Pause tracks with single play click & when ended it moves to next track
+// Note: it should works on iOS devices
 window.onload = init;
 function init() {
     document.addEventListener('play', function (e) {
         var allTracks = document.getElementsByTagName('audio');
+        var indexT = Array.prototype.indexOf.call(allTracks, e.target);
+
         for (var i = 0, len = allTracks.length; i < len; i++) {
             if (allTracks[i] != e.target) {
                 allTracks[i].pause(); // stop All Sounds except play one
             }
         }
+
         // this moves onto next track when current one ends
-        e.target.addEventListener('ended', function(){
-            var indexT = Array.prototype.indexOf.call(allTracks, e.target);
+        e.target.addEventListener('ended', function(){            
             if (indexT != null) {
                 if (indexT == allTracks.length-1) {
                     indexT = -1;
                 }
-                allTracks[indexT+1].play();
+            allTracks[indexT+1].load();
+            allTracks[indexT+1].play();
             }
         });
     }, true);
